38f1f3b0aab120b4c9cfea72ac3d16bc9978aeae
Jacob Kim <jacobjk01@berkeley.edu>
2022-10-26 15:41:26 -0700

    Initialize repo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello I'm Jacob and I like cats.
         </p>
         <a
           className="App-link"
