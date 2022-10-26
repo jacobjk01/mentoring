d9023225b75310ea3eee9fe1a43d4167cc5e8b03
Jacob Kim <jacobjk01@berkeley.edu>
2022-10-26 15:49:35 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          asdfjaslkdjfaklsj
+          I'm Lianna
         </p>
         <a
           className="App-link"
